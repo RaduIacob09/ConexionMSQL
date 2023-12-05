f2133d3c6c6756943f3795bb4c45a1153e69809e
Radu Cristian Iacob <132491462+RaduIacob09@users.noreply.github.com>
2023-12-05 12:13:41 +0100

    JCVD.java

--- BaseDatos/src/main/java/com/mycompany/basedatos/JCVD.java
+++ BaseDatos/src/main/java/com/mycompany/basedatos/JCVD.java
@@ -11,7 +11,11 @@ public class JCVD {
         try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS); 
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(QUERY);){
-            
+            /*Declaramos los datos de la base de datos para poder conectar con ella. Ya una vez
+              delcarado en el try tendremos que abrir una conexion con el metodo DriverManager.getConnection.
+              Despues de esto para establecer una conexion necesitamos el objeto Statement para poder ejecutar
+              las sentencias SQL. Y ahora el objeto ResultSet lo utilizamos para que ejecute el QUERY, es decir, 
+              la sentencia.*/
             while(rs.next()){
                 System.out.print("ID: " + rs.getInt("Id")+" ");
                 System.out.print("Nombre: " + rs.getString("Nombre")+" ");
